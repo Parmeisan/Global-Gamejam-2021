d94a4800e98fff8b26da8cabbc59a1fbd06893d7
PaulGGJ <paulsmart525@gmail.com>
2021-01-31 11:03:52 -0600

    Added monster anims, flag checks

--- godot/src/map/pawns/PawnLeader.gd
+++ godot/src/map/pawns/PawnLeader.gd
@@ -49,15 +49,26 @@ func random_encounter():
 	var second_slime = game_node.get_node("Party/Robi2")
 	var third_slime = game_node.get_node("Party/Robi3")
 	
+	var first_monster = game_node.get_node("Party/Robi4")
+	var second_monster = game_node.get_node("Party/Robi5")
+	var third_monster = game_node.get_node("Party/Robi6")
+	
 	#for testing puroses, definitely delete these flag sets if you see them:
 	#Data.setSlime(0, true)
 	#Data.setSlime(1, true)
 	#Data.setSlime(2, true)
 	#Data.setMonster(0, true)
-		
+	#Data.setMonster(1, true)
+	#Data.setMonster(2, true)
+	
 	first_slime.visible = Data.hasSlime(0) && !Data.hasMonster(0)
 	second_slime.visible = Data.hasSlime(1) && !Data.hasMonster(1)
 	third_slime.visible = Data.hasSlime(2) && !Data.hasMonster(2)
+	
+	first_monster.visible = Data.hasMonster(0)
+	second_monster.visible = Data.hasMonster(1)
+	third_monster.visible = Data.hasMonster(2)
+	
 	
 	var rnd = rand_range(0.0, 100.0)
 	if rnd < Data.curr_combat_chance:
